4bf7885d3c0c43519efd5a5df09d94b6b2e1b78f
Shareer <shareer.thaze@source.ag>
2024-10-16 10:41:39 +0200

    NR-Chapter-01

---
+++ App.js
@@ -0,0 +1,9 @@
+
+
+const parent = React.createElement("div", {id:"parent"}, [React.createElement("div", {id:"child"}, React.createElement("h1", {}, "h1 tag")), React.createElement("div", {id:"child1"}, React.createElement("h2", {}, "h2 tag"))]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(parent);
+
+root.render(parent);
